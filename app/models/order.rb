622e302e04276895137a77bada46efa5cce78a1c
Nelson Rotimi <nelsonabieno@gmail.com>
2022-06-06 09:06:51 +0100

    add query methods to order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,7 @@ class Order < ActiveRecord::Base
   belongs_to :shopper
 
   validates :amount, presence: true
+
+  scope :completed, -> { where.not(completed_at: nil) }
+  scope :date_range, -> (start_date, end_date) { where(completed_at: (start_date..end_date)) }
 end
