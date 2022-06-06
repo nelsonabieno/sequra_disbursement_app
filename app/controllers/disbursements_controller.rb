28ef05ef9c591d3579a59a94cbc65dfa429333df
Nelson Rotimi <nelsonabieno@gmail.com>
2022-06-06 09:12:14 +0100

    add functionality to calculate disbursed amount per  order amount

--- app/controllers/disbursements_controller.rb
+++ app/controllers/disbursements_controller.rb
@@ -1,2 +1,18 @@
 class DisbursementsController < ApplicationController
+  # returns the total disbursements for a given merchant
+  def index
+    disbursements = Disbursement.disbursed_amount(amount)
+    result = disbursements.disbursed_amount
+    render json: { amount: result }
+  end
+
+  private
+
+  def merchant
+    Merchant.find(disbursement_params[:merchant_id])
+  end
+
+  def disbursement_params
+    params.permit(:date, :merchant_id)
+  end
 end
--- app/models/disbursement.rb
+++ app/models/disbursement.rb
@@ -2,4 +2,15 @@ class Disbursement < ActiveRecord::Base
   belongs_to :merchant
 
   validates :disbursed_amount, presence: true
+
+  # deducts fee and calculate the disbursed amount
+  def self.disbursed_amount(amount)
+    if amount < 50
+      amount * 0.01
+    elsif amount >= 50 && amount <= 300
+      amount * 0.0095
+    else
+      amount * 0.0085
+    end
+  end
 end
